d3e8a9fcfdb34526e6285f222ed69948064de5d6
JarGad23 <j.gad@m8b.pl>
2023-10-12 19:42:47 +0200

    Moved main page to organization folder

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const MarketingPage = () => {
+  return <div>Marketing page</div>;
+};
+
+export default MarketingPage;

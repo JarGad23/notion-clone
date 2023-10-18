f51c1458bb2759221792b7605136580c1005e8cb
JarGad23 <j.gad@m8b.pl>
2023-10-18 16:13:31 +0200

    Added component to update cover image

---
+++ components/modals/cover-image-modal.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import { useParams } from "next/navigation";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import { useCoverImage } from "@/hooks/useCoverImage";
+import { SingleImageDropzone } from "@/components/SingleImageDropzone";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+export const CoverImageModal = () => {
+  const params = useParams();
+  const update = useMutation(api.documents.update);
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmiting] = useState(false);
+
+  const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmiting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+      });
+
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+
+      onClose();
+    }
+  };
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmiting(false);
+    coverImage.onClose();
+  };
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+        <SingleImageDropzone
+          className="w-full outline-none"
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
